e43792c70b43d14c1ea9a7621e2353b9b3a5c3f1
lucas <gepeoverload@gmail.com>
2022-07-27 09:59:18 +0200

    added a hitbox dom element for each meteorite

--- dist/main.js
+++ dist/main.js
@@ -229,12 +229,14 @@ class Meteorite {
   #domElement;
   #hasBeenDestroyed;
   #uid;
+  #hitbox;
   constructor(initialX, initialY) {
     this.#xPosition = initialX;
     this.#yPosition = initialY;
     this.#domElement = this.createDomElement();
     this.#hasBeenDestroyed = false;
     this.#uid = Date.now().toString(36) + Math.random().toString(36).substr(2);
+    this.#hitbox = this.createHitbox();
   }
 
   createDomElement() {
@@ -246,21 +248,34 @@ class Meteorite {
     return meteorite;
   }
 
+  createHitbox() {
+    let hitbox = document.createElement("div");
+    hitbox.className = "meteorite-hitbox";
+    hitbox.style.left = this.getXposition() + "px";
+    hitbox.style.top = this.getYposition() + "px";
+    document.getElementById("container-all").appendChild(hitbox);
+    return hitbox;
+  }
+
   startTrajectory() {
     this.checkIfOffscreen();
 
     if (!this.getHasBeenDestroyed()) {
       let domElement = this.getDomElement();
+      let hitbox = this.getHitbox();
       let currentX = this.getXposition();
       let nextX = currentX - 10;
       this.setXposition(nextX);
       domElement.style.backgroundPositionX = nextX + "px";
+      hitbox.style.left = nextX + 130 + "px";
       setTimeout(() => {
         this.startTrajectory();
       }, 100);
     } else {
       let domElement = this.getDomElement();
+      let hitbox = this.getHitbox();
       document.getElementById("container-all").removeChild(domElement);
+      document.getElementById("container-all").removeChild(hitbox);
     }
   }
 
@@ -300,6 +315,10 @@ class Meteorite {
 
   setHasBeenDestroyed(bool) {
     this.#hasBeenDestroyed = bool;
+  }
+
+  getHitbox() {
+    return this.#hitbox;
   }
 }
 
